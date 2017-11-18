72ed41ceb0d26d9876669a07be77fafceef92bd3
Yuma Hiramatsu <ayzgnm5k8y@gmail.com>
2017-11-19 00:08:27 +0900

    refactoring

---
+++ README.md
@@ -0,0 +1,4 @@
+execute below code to compile ES2016
+```
+npm run build:js
+```
--- script/main_es6.js
+++ script/main_es6.js
@@ -1,179 +1,187 @@
 (function() {
-	const id = (id) => document.getElementById(id);
+  const id = (id) => document.getElementById(id);
 
-	function getUrlQuery() {
+  const getUrlQuery = () => {
     let query_params = {};
     const url = window.location.search.slice(1).split('&');
     for (let kv of url) {
-			let [k, v] = kv.split('=');
+      let [k, v] = kv.split('=');
       query_params[k] = v;
     }
     return query_params;
-	}
+  };
 
-	// https://gist.github.com/kawanet/5553478
-	function hiraganaToKatakana(src) {
-		return src.replace(/[\u3041-\u3096]/g, function(match) {
-			const chr = match.charCodeAt(0) + 0x60;
-			return String.fromCharCode(chr);
-		});
-	}
+  // https://gist.github.com/kawanet/5553478
+  const hiraganaToKatakana = (src) => {
+    return src.replace(/[\u3041-\u3096]/g, function(match) {
+      const chr = match.charCodeAt(0) + 0x60;
+      return String.fromCharCode(chr);
+    });
+  };
 
-	// http://qiita.com/fumix/items/a7f66780c728de95558f
-	function computeDuration(ms) {
-		if (ms < 60000000) {
-	    const m = String(Math.floor((ms / 60000) + 1000)).substring(1);
-	    const s = String(Math.floor((ms - parseInt(m) * 60000) / 1000 + 100)).substring(1);
-			const ss = String(Math.floor((ms - parseInt(m) * 60000 - parseInt(s) * 1000) / 10 + 100)).substring(1);
-	    return `${m}:${s}:${ss}`;
-		} else {
-			return '999:9:99';
-		}
-	}
+  // http://qiita.com/fumix/items/a7f66780c728de95558f
+  const computeDuration = (ms) => {
+    if (ms < 60000000) {
+      const m = String(Math.floor((ms / 60000) + 1000)).substring(1);
+      const s = String(Math.floor((ms - parseInt(m) * 60000) / 1000 + 100)).substring(1);
+      const ss = String(Math.floor((ms - parseInt(m) * 60000 - parseInt(s) * 1000) / 10 + 100)).substring(1);
+      return `${m}:${s}:${ss}`;
+    } else {
+      return '999:9:99';
+    }
+  };
 
-	function runTimer() {
-		id('timer').textContent = computeDuration(Date.now() - game.startTime);
-		timerId = setTimeout(function() {
-			runTimer();
-		}, 10);
-	}
+  let timerId;
+  const runTimer = () => {
+    id('timer').textContent = computeDuration(Date.now() - game.startTime);
+    timerId = setTimeout(function() {
+      runTimer();
+    }, 10);
+  }
 
-	const ALLOWED_MISS_COUNT = 2;
-	const info = id('info');
-	const answerArea = id('answer');
-	let timerId;
+  class Game {
+    static get ALLOWED_MISS_COUNT() { return 2 }
+    static get POKEMON_RANGE() {
+      return [
+        [  1, 151],
+        [152, 251],
+        [252, 386],
+        [387, 493],
+        [494, 649],
+        [650, 721],
+        [722, 802],
+        [  1, 802]
+      ];
+    };
 
-	let pokemon_range = [
-		[  1, 151],
-		[152, 251],
-		[252, 386],
-		[387, 493],
-		[494, 649],
-		[650, 721],
-		[722, 802],
-		[  1, 802]
-	];
+    constructor(region) {
+      this.region = parseInt(region);
+      this.isStarted = false;
+      this.reset();
+    }
 
-	let game = {
-		reset(region) {
-			this.localMissCount = 0;
-			this.totalMissCount = 0;
-			this.correctCount = 0;
-			this.region = parseInt(region);
-			this.remain = pokemon_range[this.region][1] - pokemon_range[this.region][0] + 1;
-			this.numberSet = Array(this.remain).fill(0).map((v, i) => i + pokemon_range[this.region][0]);
-			this.isStart = false;
-			this.setImage(0);
-			id('remain-num').textContent = `残り${this.remain}匹`;
-			id('timer').textContent = '000:00:00';
-			this.setScoreText(0, 0);
-			info.innerHTML = '<br>';
-			answerArea.value = '';
-			answerArea.setAttribute('placeholder', 'ボタンを押してスタート');
-			id('start-btn').value = '開始'
-			answerArea.setAttribute('disabled', true);
-			clearTimeout(timerId);
-		},
+    reset() {
+      this.localMissCount = 0;
+      this.totalMissCount = 0;
+      this.correctCount = 0;
+      this.remain = Game.POKEMON_RANGE[this.region][1] - Game.POKEMON_RANGE[this.region][0] + 1;
+      this.numberSet = Array(this.remain).fill(0).map((v, i) => i + Game.POKEMON_RANGE[this.region][0]);
+      this.setImage(0);
+      id('remain-num').textContent = `残り${this.remain}匹`;
+      id('timer').textContent = '000:00:00';
+      this.setScoreText(0, 0);
+      id('info').innerHTML = '<br>';
+      id('answer').value = '';
+      id('answer').setAttribute('placeholder', 'ボタンを押してスタート');
+      id('start-btn').value = '開始'
+      id('answer').setAttribute('disabled', true);
+      clearTimeout(timerId);
+    }
 
-		determinNextNumber() {
-			const num = Math.floor(Math.random() * this.remain);
-			this.number = this.numberSet[num];
-			this.numberSet.splice(num, 1);
-			this.setImage(this.number);
-		},
+    setNewProblem() {
+      const num = Math.floor(Math.random() * this.remain);
+      this.number = this.numberSet[num];
+      this.numberSet.splice(num, 1);
+      this.setImage(this.number);
+    }
 
-		setImage(num) {
-			id('image').src = `./img/${num}.png`;
-		},
+    setImage(num) {
+      id('image').src = `./img/${num}.png`;
+    }
 
-		remainDecrement() {
-			this.remain--;
-			id('remain-num').textContent = `残り${this.remain}匹`;
-		},
+    remainDecrement() {
+      this.remain--;
+      id('remain-num').textContent = `残り${this.remain}匹`;
+    }
 
-		correctIncrement() {
-			this.correctCount++;
-			this.setScoreText();
-		},
+    correctIncrement() {
+      this.correctCount++;
+      this.setScoreText();
+    }
 
-		totalMissIncrement() {
-			this.totalMissCount++;
-			this.setScoreText();
-		},
+    totalMissIncrement() {
+      this.totalMissCount++;
+      this.setScoreText();
+    }
 
-		setScoreText() {
-			id('score').textContent = `正解${this.correctCount}匹、ミス${this.totalMissCount}匹`;
-		},
+    setScoreText() {
+      id('score').textContent = `正解${this.correctCount}匹、ミス${this.totalMissCount}匹`;
+    }
 
-		gameStart() {
-			this.isStart = true;
-			this.startTime = Date.now();
-			this.determinNextNumber();
-			answerArea.removeAttribute('disabled')
-			answerArea.setAttribute('placeholder', 'Enterで判定');
-			answerArea.focus();
-			runTimer();
-		},
+    normalization(answer) {
+      return hiraganaToKatakana(answer).replace(/２/, '2').replace(/Ｚ|z|ｚ/, 'Z').replace(/♀/, 'メス').replace(/♂/, 'オス').replace(/：/, ':').replace(/･/, '・');
+    }
 
-		gameOver() {
-			this.isStart = false;
-			clearTimeout(timerId);
-			info.textContent = '終了';
-			answerArea.setAttribute('disabled', true);
-			answerArea.setAttribute('placeholder', 'ボタンを押してスタート');
-			answerArea.value = '';
-			id('start-btn').value = '開始'
-			this.setImage(0);
-		},
+    checkAnswer() {
+      let answer = this.normalization(id('answer').value);
+      id('answer').value = '';
+      if (!this.isStarted) return false;
+      if (answer === this.answer()) {
+        if (this.localMissCount <= Game.ALLOWED_MISS_COUNT) this.correctIncrement();
+        if (this.remain === 0) {
+          this.thisOver();
+        } else {
+          id('info').innerHTML = (this.localMissCount < 3) ? '正解！' : '<br>';
+          this.localMissCount = 0;
+          this.setNewProblem();
+        }
+      } else {
+        switch (this.localMissCount) {
+          case Game.ALLOWED_MISS_COUNT:
+            id('info').textContent = `答えは「${this.answer()}」でした。`;
+            this.localMissCount++;
+            this.totalMissIncrement();
+            break;
+          case 1:
+          case 0:
+            id('info').textContent = `「${answer}」は間違いです。`;
+            this.localMissCount++;
+        }
+      }
+    }
 
-		answer() {
-			return pokemon_data[this.number-1].name;
-		}
-	};
+    gameStart() {
+      game.reset();
+      this.isStarted = true;
+      this.startTime = Date.now();
+      this.setNewProblem();
+      id('answer').removeAttribute('disabled')
+      id('answer').setAttribute('placeholder', 'Enterで判定');
+      id('answer').focus();
+      id('start-btn').value = '終了';
+      runTimer();
+    }
 
-	id('answer-form').addEventListener('submit', function(e) {
-		e.preventDefault();
-		let userAnswer = answerArea.value;
-		answerArea.value = '';
-		if (!game.isStart) return 0;
-		userAnswer = hiraganaToKatakana(userAnswer);
-		userAnswer = userAnswer.replace(/２/, '2').replace(/Ｚ|z|ｚ/, 'Z').replace(/♀/, 'メス').replace(/♂/, 'オス').replace(/：/, ':').replace(/･/, '・');
-		if (userAnswer === game.answer()) {
-			game.remainDecrement();
-			if (game.localMissCount < 3) game.correctIncrement();
-			if (game.remain === 0) {
-				game.gameOver();
-			} else {
-				info.innerHTML = (game.localMissCount < 3) ? '正解！' : '<br>';
-				game.localMissCount = 0;
-				game.determinNextNumber();
-			}
-		} else {
-			switch (game.localMissCount) {
-				case 2:
-					info.textContent = `答えは「${game.answer()}」でした。`;
-					game.localMissCount++;
-					game.totalMissIncrement();
-					break;
-				case 1:
-				case 0:
-					info.textContent = `「${userAnswer}」は間違いです。`;
-					game.localMissCount++;
-			}
-		}
-	}, false);
+    gameOver() {
+      this.isStarted = false;
+      clearTimeout(timerId);
+      id('info').textContent = '終了';
+      id('answer').setAttribute('disabled', true);
+      id('answer').setAttribute('placeholder', 'ボタンを押してスタート');
+      id('answer').value = '';
+      id('start-btn').value = '開始';
+      this.setImage(0);
+    }
 
-	id('start-btn').addEventListener('click', function(e) {
-		if (!game.isStart) {
-			game.reset(game.region);
-			this.value = '終了';
-			game.gameStart();
-		} else {
-			game.gameOver();
-		}
-	}, false);
+    answer() {
+      return pokemon_data[this.number - 1].name;
+    }
+  }
 
-	const region = getUrlQuery()['region'] || '0'
-	document.querySelector(`[data-region="${region}"]`).classList.add('active')
-	game.reset(region);
+  id('answer-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    game.checkAnswer();
+  }, false);
+
+  id('start-btn').addEventListener('click', function(e) {
+    if (!game.isStarted) {
+      game.gameStart();
+    } else {
+      game.gameOver();
+    }
+  }, false);
+
+  const region = getUrlQuery()['region'] || '0';
+  document.querySelector(`[data-region="${region}"]`).classList.add('active');
+  const game = new Game(region);
 })();
